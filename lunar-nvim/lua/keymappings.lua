584291ceb7688dbd1f544344926c64cf6f8483dd
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-09-20 15:19:51 -0300

    add keymaps to lunarvim

---
+++ lunar-nvim/lua/keymappings.lua
@@ -0,0 +1,30 @@
+vim.api.nvim_set_keymap('n', '<Sapace>', '<NOP>',{noremap = true, silent = true})
+
+vim.g.mapleader = ' ' 
+
+vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>',{noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('n', '<Leader>e', ':Lexplore<CR>',{noremap = true, silent = true})
+
+
+--Resize Window
+vim.api.nvim_set_keymap('n', '<M-l>', ':vertical resize -2<CR>',{silent = true})
+vim.api.nvim_set_keymap('n', '<M-h>', ':vertical resize +2<CR>',{silent = true})
+
+--Better Window navigator
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h',{silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l',{silent = true})
+vim.api.nvim_set_keymap('n', '<M-Left>', '<C-w>h',{silent = true})
+vim.api.nvim_set_keymap('n', '<M-Right>', '<C-w>l',{silent = true})
+
+--Better indenting
+vim.api.nvim_set_keymap('v', '<', '<gv',{noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv',{noremap = true, silent = true})
+
+--Tab switch buffers
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>',{noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>',{noremap = true, silent = true})
+
+--Move selected line in visual mode, up and dowm
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv\'',{noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv\'',{noremap = true, silent = true})
