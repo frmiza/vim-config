24ed822c29f122ed0cb801500a018fe8c636b8cc
frmiza <mizzaelsantos1@gmail.com>
2023-06-25 14:59:03 +0000

    suporte para clang adicionado

--- lua/colorschemes.lua
+++ lua/colorschemes.lua
@@ -22,9 +22,20 @@
 --})
 
 --TokyoNight
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.g.tokyonight_transparent = true
-
+require("tokyonight").setup({
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+})
 vim.cmd[[colorscheme tokyonight]]
+
